e25861baaf677d43da73ecfbd6f1c33ec45ba888
Halil Olcay <holcay92@gmail.com>
2023-03-24 22:25:35 +0300

    permissions check via DEXTER (3rdPartyLibrary) is added

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (!isLocationEnabled()) {
             Toast.makeText(
@@ -19,17 +38,87 @@ class MainActivity : AppCompatActivity() {
                 "Your location provider is turned off. Please turn it on.",
                 Toast.LENGTH_SHORT
             ).show()
-    // if not enabled, then open the location settings
+            // if not enabled, then open the location settings
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
-        }else{
-            Toast.makeText(
-                this,
-                "Your location provider is already turned on",
-                Toast.LENGTH_SHORT
-            ).show()
+        } else {
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+
+                            requestLocationData()
+                        }
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "You have denied location permission. Please allow it is mandatory.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
         }
     }
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    // When the user taps on "Settings" button, open the settings screen.
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+
+                    // This is used to identify the specific application to open
+                    // when we will be redirected to the settings screen !!!
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority =  Priority.PRIORITY_HIGH_ACCURACY
+
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+    /**
+     * A location callback object of fused location provider client where we will get the current location details.
+     */
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location? = locationResult.lastLocation
+            val latitude = mLastLocation?.latitude
+            Log.i("Current Latitude", "$latitude")
+
+            val longitude = mLastLocation?.longitude
+            Log.i("Current Longitude", "$longitude")
+        }
+    }
+
     // This function checks if the location services are enabled or not.GENERIC FUNCTION
     private fun isLocationEnabled(): Boolean {
         // This provides access to the system location services.
@@ -39,6 +128,9 @@ class MainActivity : AppCompatActivity() {
                 || locationManager.isProviderEnabled(
             LocationManager.NETWORK_PROVIDER
         )
+    }
+    companion object {
+        private const val REQUEST_CHECK_SETTINGS = 1
     }
 
 
